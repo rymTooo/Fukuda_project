d0aeb692fe49f44435036e0705553c2638b31968
Your Name <patt.pootrakul@gmail.com>
2023-11-04 13:45:37 +0700

    recreate auth_app

--- auth_app/apps.py
+++ auth_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthConfig(AppConfig):
+class AuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth'
+    name = 'auth_app'
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login ():
+    return 0
+
+
+def signup ():
+    return 1
