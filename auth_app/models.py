134491dd766f99b54f7d89f0090c26c4ec6c17e8
SaddestRosemary <gus.danupat@gmail.com>
2023-11-04 16:09:40 +0700

    Stat and setting

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Stat(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    all_time_money = models.IntegerField()
+    passiveincome = models.IntegerField()
+    current_money = models.IntegerField()
+    money_per_click = models.IntegerField()
+    click_counter = models.IntegerField()
+
+class Setting(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    theme = models.CharField(max_length=255)
+    sound_volumn = models.DecimalField(max_digits=3, decimal_places=1)
+    notification = models.BooleanField()
+
+      
