c671eb42e18fb605d6649d825e018428fb5b7e56
Your Name <patt.pootrakul@gmail.com>
2023-11-10 14:27:05 +0700

    static config done
    see example in login.html or signup.html

--- Fukuda/settings.py
+++ Fukuda/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,12 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
 
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    "C:/Users/pattp/Fukuda_project/static"
 ]
 
 # Default primary key field type
