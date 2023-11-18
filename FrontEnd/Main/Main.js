6fcb2399e5e6d21bfc55a8ec6c64066662e83e3c
coolponn <pontong25052@gmail.com>
2023-11-19 01:11:00 +0700

    Stat

--- FrontEnd/Main/Main.js
+++ FrontEnd/Main/Main.js
@@ -1,10 +1,31 @@
 function showTab(tabId) {
     const tabs = document.querySelectorAll('.tabContent');
-    tabs.forEach(tab => tab.classList.remove('active'));
+    tabs.forEach(tab => (tab.style.display = 'none'));
+
     const selectedTab = document.getElementById(tabId);
-    selectedTab.classList.add('active');
+    selectedTab.style.display = 'flex';
+    if (tabId === 'statistics') {
+        updateStatistics();
+    }
 }
 
+function updateStatistics() {
+    // Example variables, replace them with your actual variables
+    const currentMoney = 1000;
+    const allTimeClicks = 500;
+    const allTimeMoney = 1500;
+    const passiveIncome = 200;
+    const moneyPerClick = 2;
+
+    // Update the content of each statistic element
+    document.getElementById('CurMoneyStat').textContent = currentMoney;
+    document.getElementById('AllClickStat').textContent = allTimeClicks;
+    document.getElementById('AllMoneyStat').textContent = allTimeMoney;
+    document.getElementById('PassiveStat').textContent = passiveIncome;
+    document.getElementById('PerClickStat').textContent = moneyPerClick;
+}
+
+//Swing Animation
 const swordman = document.getElementById('swordman');
 
 hitbox.addEventListener('click', () => {
