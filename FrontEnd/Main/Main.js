e1f6f3d07f8f12ba164e0ce4968e56d309764f8c
coolponn <pontong25052@gmail.com>
2023-11-20 00:10:58 +0700

    powerups
    interface

--- FrontEnd/Main/Main.js
+++ FrontEnd/Main/Main.js
@@ -20,6 +20,31 @@ function showTab(tabId) {
     if (tabId === 'statistics') {
         updateStatistics();
     }
+    if (tabId === 'powerups') {
+        updatePower();
+    }
+}
+
+function updatePower() {
+    // Example variables, replace them with your actual variables
+    let pow1Price = 10;
+    let pow2Price = 50;
+    let pow3Price = 100;
+    let pow4Price = 500;
+    let pow1Name = "Oil";
+    let pow2Name = "Titanium";
+    let pow3Name = "Grinder";
+    let pow4Name = "Cloud";
+
+    // Update the content of each statistic element
+    document.getElementById('pow1Name').textContent = pow1Name;
+    document.getElementById('pow2Name').textContent = pow2Name;
+    document.getElementById('pow3Name').textContent = pow3Name;
+    document.getElementById('pow4Name').textContent = pow4Name;
+    document.getElementById('pow1Price').textContent = pow1Price;
+    document.getElementById('pow2Price').textContent = pow2Price;
+    document.getElementById('pow3Price').textContent = pow3Price;
+    document.getElementById('pow4Price').textContent = pow4Price;
 }
 
 function updateStatistics() {
