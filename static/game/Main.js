7ebf978bccd2f2cd8078538a09c85eb5ac70dd90
Your Name <patt.pootrakul@gmail.com>
2023-11-30 00:19:58 +0700

    update main.js

--- static/game/Main.js
+++ static/game/Main.js
@@ -22,6 +22,8 @@ let skills = [];// skill list composes of many skill objects.
 let powers = [];
 let powerInShop = [];
 let target_id = 0;
+let customisationOptions={};
+let selectedCustomisation = {head : "head/headDefault.png", torso:"torso/torsoDefault.png",pants:"pants/pantsDefault.png",shoes:"shoes2/shoes2Default.png"};//load from db
 
 let theme = "../../static/game/Background.png"
 get_data();
@@ -42,6 +44,7 @@ let SwordSound = "../../static/game/Sword1.mp3"
 fetchPowers();// should move into get data method and then just run get data method once.
 fetchSkills();
 initializeSettings();
+fetchCustomisation();
 
 // Get the audio element
 const backgroundAudio = document.getElementById('backgroundAudio');
@@ -53,6 +56,12 @@ backgroundAudio.play();
 document.getElementById('money').textContent = Math.floor(cur_money);
 document.addEventListener('DOMContentLoaded', fetchSkills);
 document.addEventListener('DOMContentLoaded', fetchPowers);
+document.addEventListener('DOMContentLoaded', fetchCustomisation);
+
+headIdle = document.getElementById('headIdle');
+torsoIdle = document.getElementById('torsoIdle');
+pantsIdle = document.getElementById('pantsIdle');
+shoesIdle = document.getElementById('shoesIdle');
 
 //Clicking
 const swordman = document.getElementById('swordman');
@@ -83,8 +92,13 @@ hitbox.addEventListener('click', () => {
 
     playRandomSound();
     gaugeValue+= 5;
+    headIdle.style.visibility = 'hidden';
+    torsoIdle.style.visibility = 'hidden';
+    pantsIdle.style.visibility = 'hidden';
+    shoesIdle.style.visibility = 'hidden';
     setTimeout(() => {
         swordman.classList.remove('SwingAnim');
+        updateCustomisations();
         
     }, 700); // Adjust based on your swing animation duration
     
@@ -400,8 +414,37 @@ function buyPowerUp(powerUp) {
 }
 
 function updateStatistics() {
+    players.sort((a, b) => b.all_time_money - a.all_time_money);
+    const leaderboardBody = document.getElementById('leaderboardBody');
+    // Clear existing content
+    leaderboardBody.innerHTML = '';
+    let top5 =[];
+    console.log(players);
+    // Display players in the table
+    for (let i = 0; i < Math.min(players.length, 5); i++) {
+        const player = players[i];
+        top5.push(player);
+        const rowClass = player.id === OurPlayerid ? 'highlight' : '';
+        const row = `<tr class="${rowClass}">
+                      <td>${i + 1}</td>
+                      <td>${player.name}</td>
+                      <td>${player.all_time_money}</td>
+                    </tr>`;
+        leaderboardBody.innerHTML += row;
+      }
+    // Display your data in row 6 if you're not in the top 5
+    if (top5.findIndex(player => player.id === OurPlayerid) === -1) {
+        const rank = players.findIndex(player => player.id === OurPlayerid);
+        const yourData = players.find(player => player.id === OurPlayerid);
+        const yourDataRow = `<tr class="highlight">
+                              <td>${rank+1}</td>
+                              <td>${yourData.name}</td>
+                              <td>${yourData.all_time_money}</td>
+                            </tr>`;
+        leaderboardBody.innerHTML += yourDataRow;
+      }
       // Update the content of each statistic element
-    document.getElementById('CurMoneyStat').textContent = Math.floor(cur_money);
+
     document.getElementById('AllClickStat').textContent = click_counter;
     document.getElementById('AllMoneyStat').textContent = Math.floor(all_time_money);
     document.getElementById('PassiveStat').textContent = Math.floor(totalPassiveIncome);
@@ -433,6 +476,84 @@ function changeUsername() {
         alert("Username is Changed!!");
     }
 }
+
+function updateCustomisations(){
+    if(selectedCustomisation["head"] == customisationOptions["head"][0]){
+        headIdle.style.visibility = 'hidden';
+    }else{
+        console.log("show " + selectedCustomisation["head"])
+        console.log("show " + customisationOptions["head"][0])
+        headIdle.style.visibility = 'visible';
+        headIdle.src = selectedCustomisation["head"];
+    }
+    if(selectedCustomisation["torso"] == customisationOptions["torso"][0]){
+        torsoIdle.style.visibility = 'hidden';
+    }else{
+        torsoIdle.style.visibility = 'visible';
+        torsoIdle.src = selectedCustomisation["torso"];
+    }
+    if(selectedCustomisation["pants"] == customisationOptions["pants"][0]){
+        pantsIdle.style.visibility = 'hidden';
+    }else{
+        pantsIdle.style.visibility = 'visible';
+        pantsIdle.src = selectedCustomisation["pants"];
+    }
+    if(selectedCustomisation["shoes"] == customisationOptions["shoes"][0]){
+        shoesIdle.style.visibility = 'hidden';
+    }else{
+        shoesIdle.style.visibility = 'visible';
+        shoesIdle.src = selectedCustomisation["shoes"];
+    }
+    console.log(selectedCustomisation["head"]);
+};
+
+function fetchCustomisation(){
+    customisationOptions = {
+    head: ['head/headDefault.png', 'head/headPink.png', 'head/headBlue.png', 'head/headGreen.png', 'head/headYellow.png', 'head/headPurple.png', 'head/headOrange.png'],
+    torso: ['torso/torsoDefault.png', 'torso/torsoPink.png', 'torso/torsoBlue.png', 'torso/torsoGreen.png', 'torso/torsoYellow.png', 'torso/torsoPurple.png', 'torso/torsoOrange.png'],
+    pants: ['pants/pantsDefault.png', 'pants/pantsPink.png', 'pants/pantsBlue.png', 'pants/pantsGreen.png', 'pants/pantsYellow.png', 'pants/pantsPurple.png', 'pants/pantsOrange.png'],
+    shoes: ['shoes2/shoes2Default.png', 'shoes2/shoes2Pink.png', 'shoes2/shoes2Blue.png', 'shoes2/shoes2Green.png', 'shoes2/shoes2Yellow.png', 'shoes2/shoes2Purple.png', 'shoes2/shoes2Orange.png']
+  };
+  updateCustomisations();
+}
+
+function changeCustomisation(part, direction) {
+    const column = document.getElementById(`${part}Column`);
+    const image = document.getElementById(`${part}Image`);
+    const options = customisationOptions[part];
+    console.log("Current Pic " + image.src);
+    let currentIndex = options.indexOf(image.src.replace('http://127.0.0.1:5501/FrontEnd/Main/',''));
+    console.log("Current" + currentIndex)
+    if (direction === 'left') {
+      currentIndex = (currentIndex - 1 + options.length) % options.length;
+    } else if (direction === 'right') {
+      currentIndex = (currentIndex + 1)% options.length;
+    }
+    console.log("After" + currentIndex)
+    console.log(options[currentIndex]);
+    console.log(options[5]);
+    selectedCustomisation[part] = options[currentIndex];
+    updateCustomisations();
+    image.src = options[currentIndex];
+
+
+}
+
+let players = [
+    { id: 1, name: 'Player1', all_time_money: 500 },
+    { id: 2, name: 'Player2', all_time_money: 1000 },
+    { id: 3, name: 'Player3', all_time_money: 1500 },
+    { id: 4, name: 'Player4', all_time_money: 2000 },
+    { id: 5, name: 'Player5', all_time_money: 2500 },
+    { id: 6, name: 'Player6', all_time_money: 3500 },
+    { id: 7, name: 'Player7', all_time_money: 5500 },
+    { id: 8, name: 'Player8', all_time_money: 6500 },
+    { id: 9, name: 'Player9', all_time_money: 7500 },
+    // Add more players...
+];
+let OurPlayerid = 1;
+
+
 //Clicking
 
 
